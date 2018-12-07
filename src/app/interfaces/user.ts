f574efe573f2c9ef74950dbe6e746964e3873f59
Ricardo Portillo <ricardo.portillo@meduca.gob.pa>
2018-12-07 14:39:41 -0500

    cambios css

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -7,4 +7,5 @@ export interface User {
     email: string;
     friend: boolean;
     uid: any;
+    status: string;
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,41 +6,46 @@ import { User } from '../interfaces/user';
 })
 export class UserService {
   friends:User[];
-  constructor() { 
+  constructor() {
     let usuario1: User = {
       nick: 'Eduardo',
       age: 24,
       email: 'ed@aoe.aoe',
       friend: true,
-      uid: 1
+      uid: 1,
+      status: 'online'
     };
     let usuario2:User = {
       nick: 'Freddy',
       age: 28,
       email: 'fred@aoe.aoe',
       friend: true,
-      uid: 2
+      uid: 2,
+      status: 'offline'
     };
     let usuario3: User = {
       nick: 'Yuliana',
       age: 18,
       email: 'yuli@aoe.aoe',
       friend: true,
-      uid: 3
+      uid: 3,
+      status: 'busy'
     };
     let usuario4: User = {
       nick: 'Ricardo',
       age: 17,
       email: 'rick@aoe.aoe',
       friend: false,
-      uid: 4
+      uid: 4,
+      status: 'offline'
     };
     let usuario5: User = {
       nick: 'Marcos',
       age: 30,
       email: 'marcos@aoe.aoe',
       friend: false,
-      uid: 5
+      uid: 5,
+      status: 'offline'
     };
     this.friends = [
       usuario1, usuario2, usuario3, usuario4, usuario5
